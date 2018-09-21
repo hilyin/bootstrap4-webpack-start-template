de1fbc3658601d60a449643689eed2fae77b2882
Graham Mickle <hilyin@users.noreply.github.com>
2018-09-20 18:30:58 -0700

    bootstrap 4 webpack start template

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+/* Styling */
+import "../scss/_custom.scss";
+import "bootstrap";
+import "../scss/app.scss";
+
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        'app': './src/js/app.js',
+    },
+    output: {
+        filename: './js/[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
